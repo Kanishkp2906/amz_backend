2adb737df798bcbb7f9bffc4b8960ac8e91b1f30
Kanishk@29 <kanishkprasad6@gmail.com>
2026-01-31 19:41:55 +0530

    added the environment origins for the frontend, which I actually forget to add previously.

--- middleware/app_cors.py
+++ middleware/app_cors.py
@@ -1,10 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
+from config import ORIGINS
 
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    ORIGINS
 ]
 
 def add_cors_middleware(app):
